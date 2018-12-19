e48792ae36049b4007a90cc9f9d7e16d2bf4f2a0
Propilideno <lucas@anjosdafoto.com.br>
2018-12-19 06:10:48 -0200

    v1.1 Funciona perfeitamente, porem tem que ser 2 cadastramentos no maximo

--- cadastrador.c
+++ cadastrador.c
@@ -5,10 +5,9 @@
 #include <windows.h>
 int main()
 {
-    int tpag,ipag,x=1,progresso,j=0,i;
+    int tpag,ipag,x=1,progresso,j=0,i,u=0;
     float tempo_leitura,tempo_individual;
-    char sprogresso[12];
-    char *livro;
+    char sprogresso[12],livro[50];
     time_t rawtime;
     struct tm * timeinfo;
     time ( &rawtime );
@@ -18,6 +17,7 @@ int main()
     printf("===== PLANEJAMENTO DE LEITURAS =====\n");
     printf("    Digite 1 para cadastrar o livro.\n");
     printf("    Digite 2 para marcar a pagina que parou do livro. (EM DESENVOLVIMENTO)\n");
+    printf("    Digite 3 para ver as instrucoes de uso do programa.\n");
     printf("    Digite 0 para sair.\n");
     while(x != 0)
     {
@@ -26,9 +26,8 @@ int main()
         switch(x)
         {
             case 1:
-                livro = (char*) malloc(50 * sizeof(char));
                 printf("Digite aqui o nome do livro que deseja cadastrar: ");
-                scanf(" %s", livro);
+                scanf(" %s",livro);
                 printf("Digite quantas paginas o livro tem: ");
                 scanf("%d",&tpag);
                 printf("Digite em que pagina do livro voce esta: ",&ipag);
@@ -39,7 +38,7 @@ int main()
                 scanf("%f",&tempo_leitura);
                 p = fopen("livros.txt","a");
                 progresso = (float) 100* ipag / tpag;
-                sprintf(sprogresso,"%d",progresso);
+                sprintf(sprogresso,"%.2f",progresso);
                 strcat(sprogresso,"%");
                 fprintf(p,"- %s:\n\n      Inicio da Leitura: %s      Paginas lidas: %d\n      Paginas totais: %d\n      Progresso: %s\n\n",livro,asctime (timeinfo),ipag,tpag,sprogresso);
                 float tempos_sugeridos[16],paginas_por_dia[16],paginas_por_hora[16];
@@ -68,19 +67,38 @@ int main()
                         fprintf(p,"      - %.1f dias (Tempo determinado pelo usuário):\n        Para ler o livro em %.1f dias voce devera ler %.1f paginas por dia ou dentro do seu planejamento diário com (%.1f horas/dia) teriam de ser lidas %.1f paginas por hora.\n\n",tempos_sugeridos[i],tempos_sugeridos[i],paginas_por_dia[i],tempo_leitura,paginas_por_hora[i]);
                     }
                 }
-                fprintf(p,"      - Recomendado pelo autor (%.1f dias):\n        Para ler o livro em %.1f dias você devera ler 30.0 paginas por dia ou dentro do seu planejamento diário com (%.1f horas/dia) teriam de ser lidas %.1f paginas por hora dentro de %.1f horas.\n\n", autor_dias,autor_dias,tempo_leitura,autor_pag_por_hora,tempo_leitura);
-
-                printf("Parabens, voce cadastrou o livro: %s.\n", livro);
-                free(livro);
+                fprintf(p,"      - Recomendado pelo autor (%.1f dias):\n        Para ler o livro em %.1f dias você devera ler 30.0 paginas por dia ou dentro do seu planejamento diário com (%.1f horas/dia) teriam de ser lidas %.1f paginas por hora.\n\n\n", autor_dias,autor_dias,tempo_leitura,autor_pag_por_hora);
+                Sleep(300);
+                printf("\nProcessando .");
+                Sleep(400);
+                printf(" .");
+                Sleep(500);
+                printf(" .");
+                printf("\nLivro cadastrado com sucesso!\n");
                 break;
             case 2:
-                printf("Ainda estamos desenvolvendo esse espaco, peco que tenha paciencia e aguarde!");
+                printf("Ainda estamos desenvolvendo esse espaco, peco que tenha paciencia e aguarde!\n");
+                break;
+            case 3:
+                system("cls");
+                printf("Digitos:\n\n  1: Voce podera cadastrar um livro desejado para a leitura.\n      Para cadastrar o livro, basta digitar o nome do livro.\n      Exemplo: Certo -> Algebra_Linear ou Algebra-Linear ou AlgebraLinear.\n               Errado -> Algebra Linear.\n      Deves digitar o nome sem usar espaco!\n      Logo depois as infomacoes que irao lhe pedir, nao ha misterios alem deste!\n\n");
+                printf("  2: Voce podera comparar seu progresso atual com o progresso que voce\n     deveria estar cumprindo atraves de cadastramentos das paginas atuais.\n     {\n          EM DESENVOLVIMENTO\n     }\n\n");
+                printf("  3: Se voce esta lendo isso agora, creio que voce ja sabe usar o digito 3.\n\n");
+                printf("  0: Caso tenha feito seus cadastramentos e acompanhamentos, digite 0 para sair.\n\n");
+                printf("  Qualquer outro digito sem ser os ditos: Ocorrera uma mensagem pedindo para\n                                          voce digitar um numero valido.\n\n");
+                system("pause");
+                system("cls");
+                printf("===== PLANEJAMENTO DE LEITURAS =====\n");
+                printf("    Digite 1 para cadastrar o livro.\n");
+                printf("    Digite 2 para marcar a pagina que parou do livro. (EM DESENVOLVIMENTO)\n");
+                printf("    Digite 3 para ver as instrucoes de uso do programa.\n");
+                printf("    Digite 0 para sair.\n");
                 break;
             case 0:
                 exit(1);
                 break;
             default:
-                printf("Por favor, digite uma opcao valida!");
+                printf("Por favor, digite uma opcao valida!\n");
                 break;
         }
     }
